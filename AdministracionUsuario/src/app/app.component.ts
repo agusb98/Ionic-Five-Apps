8b41e2ea2436d186201fac6ce944090a66daef28
agusb98 <agusszurdob@gmail.com>
2021-04-16 23:45:24 -0300

    animations with animista

--- AdministracionUsuario/src/app/app.component.ts
+++ AdministracionUsuario/src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Router } from '@angular/router';
+import { Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
+  providers: [StatusBar],
 })
 export class AppComponent {
-  constructor() {}
+  constructor(
+    private platform: Platform,
+    private statusBar: StatusBar,
+    public router: Router
+  ) {
+    this.initializeApp();
+  }
+
+  initializeApp() {
+    this.platform.ready().then(() =>{
+      this.statusBar.styleDefault();
+      this.router.navigateByUrl('splash');
+    });
+  }
 }
---
+++ AlarmaRobo/src/app/pages/splash/splash.page.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-splash',
+  templateUrl: './splash.page.html',
+  styleUrls: ['./splash.page.scss'],
+})
+export class SplashPage {
+
+  constructor(
+    public router: Router
+  ) {
+    setTimeout(() => {
+      this.router.navigateByUrl('home');
+    }, 2000);
+  }
+}
